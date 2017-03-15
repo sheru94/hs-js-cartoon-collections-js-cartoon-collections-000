ce3f588c8ea50d14d141405ca0dda1e8fc884882
Shamsher Singh <singhs94@hotmail.com>
2017-03-15 19:25:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 function dwarfRollCall(dwarves) {
+  var string = ""
+  for (var i = 0; i < dwarves.length; i++) {
+    string = string + (i + 1) + ". " + dwarves[i] + " "
+  }
+  return string;
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var array = planeteerCalls.map(function(el) {
+    return el.toUpperCase() + "!"
+  });
+  return array;
 }
 
 function longPlaneteerCalls(words) {
+  for (var i = 0; i < words.length; i++) {
+    if (words[i].length > 4) {
+      return true;
+    }
+  }
+  return false;
 }
 
 function findTheCheese (foods) {
+  var cheeses = ["cheddar","gouda","camembert"]
+
+  for (var i = 0; i < foods.length; i++) {
+    if (cheeses.includes(foods[i])) {
+      return foods[i]
+    }
+  }
+  return "no cheese!"
 }
